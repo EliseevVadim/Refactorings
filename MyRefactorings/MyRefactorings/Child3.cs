23e856664d8b5a611e512bd7821535212393799e
EliseevVadim <55801514+EliseevVadim@users.noreply.github.com>
2020-12-16 22:06:47 +0300

    9й рефакторинг - замена наследования делегированием

--- MyRefactorings/MyRefactorings/Child3.cs
+++ MyRefactorings/MyRefactorings/Child3.cs
@@ -4,11 +4,21 @@ using System.Text;
 
 namespace MyRefactorings
 {
-    class Child3:Parent
+    class Child3
     {
+
+        public Child3()
+        {
+            delegated_parent = new Parent();
+        }
         public void PrintInfo()
         {
             Console.WriteLine("I'm only for child 3!!!!!");
         }
+        public void ShowMessage()
+        {
+            delegated_parent.ShowMessage();
+        }
+        private Parent delegated_parent;
     }
 }
