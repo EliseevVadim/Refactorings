b30c264e797e502e86da0436d1e009de24399c3c
EliseevVadim <55801514+EliseevVadim@users.noreply.github.com>
2020-12-16 18:46:00 +0300

    first bad code

---
+++ MyRefactorings/MyRefactorings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyRefactorings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            int hours = dt.Hour;
+            int mins = dt.Minute;
+            int secs = dt.Second;
+            Console.WriteLine($"Сейчас:\n{hours} часов\n{mins} минут\n{secs} секунд");
+        }
+    }
+}
